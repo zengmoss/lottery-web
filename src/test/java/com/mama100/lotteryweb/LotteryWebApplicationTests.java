83dbaa5e1edac897558f339b6d2bc1b485893c23
zengmoss <80502321@qq.com>
2022-08-16 17:24:40 +0800

    重新生成文件

--- src/test/java/com/mama100/lotteryweb/LotteryWebApplicationTests.java
+++ src/test/java/com/mama100/lotteryweb/LotteryWebApplicationTests.java
@@ -2,12 +2,18 @@ package com.mama100.lotteryweb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+@ActiveProfiles("dev")
+@SpringBootTest()
 class LotteryWebApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("begin");
+        new RestTemplate().getForObject("http://localhost:8020/o2o-web/test/hello",String.class);
+        System.out.println("end");
     }
 
 }
